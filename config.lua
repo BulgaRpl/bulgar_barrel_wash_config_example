2ccef4251139104f1fcc4119f55be5e1cdfa979f
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2023-11-21 19:45:20 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,37 @@
+-- This is just an example of the config for the bulgar_barrel_wash script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_barrel_wash script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_barrel_wash script
+-- In the example config some settings have been removed that you will get only after getting the script 
+-- This is just an example of the config for the bulgar_barrel_wash script
+-- In the example config some settings have been removed that you will get only after getting the script 
+
+Config = {}
+
+-- Water Barrels Hashes
+Config.Barrels = {
+	000, -- 'CONFIGURED AFTER U GET THE SCRIPT'
+	000, -- 'CONFIGURED AFTER U GET THE SCRIPT'
+}
+
+-- Prompts
+Config.AlwaysShowPrompts = false -- If "false" player must hold "G" key to show prompts. / If "true" prompts will be always visible.
+
+-- Keys
+Config.HoldKeyToShowPrompts = 0x760A9C6F -- G
+Config.StartWashKey = 0xF84FA74F -- MOUSE2
+
+-- Clear Ped Function
+Config.ClearPed = function()
+	-- CONFIGURED AFTER U GET THE SCRIPT
+end
+
+-- Play Sound When Cleared
+Config.PlaySound = true
+
+-- Translations
+Config.Language = {
+    [1] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+    [2] = {text = "CONFIGURED AFTER U GET THE SCRIPT",},
+}
